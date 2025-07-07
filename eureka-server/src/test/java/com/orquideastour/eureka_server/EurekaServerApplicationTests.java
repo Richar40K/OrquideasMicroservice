6fc8bc1cc4d344eaca7105d407386b0d6bc3ef8b
Richar40K <ricardoismael777@gmail.com>
2025-07-07 15:41:17 -0500

    aggregating microservices

---
+++ eureka-server/src/test/java/com/orquideastour/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orquideastour.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
