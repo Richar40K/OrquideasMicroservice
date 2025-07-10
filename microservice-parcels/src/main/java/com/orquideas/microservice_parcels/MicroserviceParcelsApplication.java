ad5ccc9111dd77953c8bb28714a483070428db50
Richar40K <ricardoismael777@gmail.com>
2025-07-10 12:41:30 -0500

    microservice-parcels

---
+++ microservice-parcels/src/main/java/com/orquideas/microservice_parcels/MicroserviceParcelsApplication.java
@@ -0,0 +1,13 @@
+package com.orquideas.microservice_parcels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceParcelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceParcelsApplication.class, args);
+	}
+
+}
---
+++ microservice-parcels/src/test/java/com/orquideas/microservice_parcels/MicroserviceParcelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orquideas.microservice_parcels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceParcelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
