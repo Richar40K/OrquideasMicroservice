1ca67c6eb1878fbebafefc6f716d2f269676bddb
jackutp <u21318614@utp.edu.pe>
2025-07-17 10:35:59 -0500

    modificar lombok delete y reescribir los get and set

--- microservice-users/src/main/java/com/orquideastour/microservice_users/entities/Role.java
+++ microservice-users/src/main/java/com/orquideastour/microservice_users/entities/Role.java
@@ -1,17 +1,30 @@
 package com.orquideastour.microservice_users.entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Table(name="roles")
-@Getter
-@Setter
+
 public class Role
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- microservice-users/src/main/java/com/orquideastour/microservice_users/entities/User.java
+++ microservice-users/src/main/java/com/orquideastour/microservice_users/entities/User.java
@@ -8,14 +8,9 @@ import com.orquideastour.microservice_users.enums.State;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.List;
 @Entity
 @Table(name = "users")
-@Getter
-@Setter
 public class User
 {
     @Id
@@ -82,5 +77,166 @@ public class User
 
     public String generateCodigo(long numero) {
         return String.format("COL-%03d", numero);
+    }
+
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getNameEmergency() {
+        return nameEmergency;
+    }
+
+    public void setNameEmergency(String nameEmergency) {
+        this.nameEmergency = nameEmergency;
+    }
+
+    public String getPhoneEmergency() {
+        return phoneEmergency;
+    }
+
+    public void setPhoneEmergency(String phoneEmergency) {
+        this.phoneEmergency = phoneEmergency;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCellPhone() {
+        return cellPhone;
+    }
+
+    public void setCellPhone(String cellPhone) {
+        this.cellPhone = cellPhone;
+    }
+
+    public Position getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(Position puesto) {
+        this.puesto = puesto;
+    }
+
+    public Department getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Department departamento) {
+        this.departamento = departamento;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    public State getEstado() {
+        return estado;
+    }
+
+    public void setEstado(State estado) {
+        this.estado = estado;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Boolean getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        this.admin = admin;
     }
 }
