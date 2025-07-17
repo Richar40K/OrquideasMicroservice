e7e9bddfd0ea9487c1c54c89bf120287239caf49
jackutp <u21318614@utp.edu.pe>
2025-07-17 11:23:24 -0500

    bus get and set

--- microservice-bus/src/main/java/com/orquideas/microservice_bus/entities/Bus.java
+++ microservice-bus/src/main/java/com/orquideas/microservice_bus/entities/Bus.java
@@ -3,13 +3,10 @@ package com.orquideas.microservice_bus.entities;
 import com.orquideas.microservice_bus.enums.State;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Table(name = "bus")
-@Getter
-@Setter
+
 public class Bus
 {
     @Id
@@ -28,4 +25,43 @@ public class Bus
     @Column(nullable = false)
     private State estado;
 
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Long getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(Long capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public State getEstado() {
+        return estado;
+    }
+
+    public void setEstado(State estado) {
+        this.estado = estado;
+    }
 }
