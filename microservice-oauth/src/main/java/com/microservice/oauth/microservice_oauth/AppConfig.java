c40ffeb2ffe61280c1bcdf78e11c849d073b0a4c
Richar40K <ricardoismael777@gmail.com>
2025-07-17 23:05:40 -0500

    pro fin

--- microservice-oauth/src/main/java/com/microservice/oauth/microservice_oauth/AppConfig.java
+++ microservice-oauth/src/main/java/com/microservice/oauth/microservice_oauth/AppConfig.java
@@ -14,7 +14,7 @@ public class AppConfig {
     @LoadBalanced
     public WebClient.Builder loadBalancedWebClientBuilder() {
         return WebClient.builder()
-                .baseUrl("https://microservice-users-hzyz.onrender.com") // Nombre del servicio registrado en Eureka
+                .baseUrl("https://microservice-users") // Nombre del servicio registrado en Eureka
                 .defaultHeader("Accept", "application/json")
                 .defaultHeader("Content-Type", "application/json");
     }
@@ -27,7 +27,7 @@ public class AppConfig {
     @Bean
     public WebClient usersWebClient(WebClient.Builder loadBalancedWebClientBuilder) {
         return loadBalancedWebClientBuilder
-                .baseUrl("https://microservice-users-hzyz.onrender.com")
+                .baseUrl("https://microservice-users")
                 .build();
     }
 }
