b4a93902a35ed04edcde56ff2264d07ea44112be
Richar40K <ricardoismael777@gmail.com>
2025-07-18 18:47:52 -0500

    la fe}

--- microservice-oauth/src/main/java/com/microservice/oauth/microservice_oauth/security/SecurityConfig.java
+++ microservice-oauth/src/main/java/com/microservice/oauth/microservice_oauth/security/SecurityConfig.java
@@ -7,9 +7,9 @@ import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,11 +17,9 @@ import org.springframework.security.oauth2.core.*;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.oauth2.server.authorization.client.*;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
@@ -51,6 +49,7 @@ public class SecurityConfig {
                                 "/login",
                                 "/actuator/**"
                         ).permitAll()
+                        .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwtSpec -> jwtSpec.jwtDecoder(jwtDecoder))
@@ -76,7 +75,7 @@ public class SecurityConfig {
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                .redirectUri("https://orquideas-frontend-44rifki5j-ricardos-projects-59aad0ef.vercel.app/api/oauth2/callback")
+                .redirectUri("http://localhost:3000/api/oauth2/callback")
                 .postLogoutRedirectUri("http://127.0.0.1:8090/logout")
                 .scope(OidcScopes.OPENID)
                 .scope(OidcScopes.PROFILE)
@@ -114,7 +113,7 @@ public class SecurityConfig {
     public ReactiveJwtDecoder reactiveJwtDecoder() {
         // Para desarrollo, puedes probar con IP en lugar de localhost
         return NimbusReactiveJwtDecoder
-                .withJwkSetUri("https://microservice-oauth-vn7p.onrender.com/.well-known/jwks.json")
+                .withJwkSetUri("http://127.0.0.1:9100/.well-known/jwks.json")
                 .build();
     }
 
